e553c42689e763efc0c4ebe46a510213fca0324c
Leonid Ryabokon <l.ryabokon@idaproject.com>
2023-04-08 21:44:51 +0500

    feat(bit_accessor) refactor validation function

--- module_base/bitAccessor.js
+++ module_base/bitAccessor.js
@@ -3,40 +3,29 @@ class BitAccessor {
         this.uint8Array = uint8Array;
     }
 
-    isValid(byteIdx, bitIdx, val) {
-        let isValid = true;
+    validate(byteIdx, bitIdx, val) {
         if (byteIdx < 0 || byteIdx >= this.uint8Array.length ) {
-            console.log('Array index out of range');
-            isValid = false;
+            throw new Error('Array index out of range');
         }
         if (bitIdx > 7) {
-            console.log('The bit index must be less than or equal to 7.');
-            isValid = false;
+            throw new Error('The bit index must be less than or equal to 7.');
         }
         if (bitIdx < 0) {
-            console.log('The bit index must be more than or equal to 0.');
-            isValid = false;
+            throw new Error('The bit index must be more than or equal to 0.');
         }
         if (val !== undefined && val !== 0 && val !== 1) {
-            console.log('Value must be 0 or 1.');
-            isValid = false;
+            throw new Error('Value must be 0 or 1.');
         }
-
-        return isValid;
     };
 
     get(byteIdx, bitIdx) {
-        if (this.isValid(byteIdx, bitIdx)) {
-            return Number((this.uint8Array[byteIdx] & (1 << bitIdx)) !== 0);
-        }
+        this.validate(byteIdx, bitIdx)
+        return Number((this.uint8Array[byteIdx] & (1 << bitIdx)) !== 0);
     };
 
     set(byteIdx, bitIdx, val) {
-        if (this.isValid(byteIdx, bitIdx, val)) {
-            val === 1
-                ? this.setBit(byteIdx, bitIdx)
-                : this.resetBit(byteIdx, bitIdx);
-        }
+        this.validate(byteIdx, bitIdx, val)
+        val === 1 ? this.setBit(byteIdx, bitIdx) : this.resetBit(byteIdx, bitIdx);
     };
 
     setBit(byteIdx, bitIdx) {
