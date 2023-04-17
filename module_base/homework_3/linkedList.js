db2bcc02d0bae8cd3ab00f76c5432ff17cd28e88
Leonid Ryabokon <l.ryabokon@idaproject.com>
2023-04-17 22:21:46 +0500

    feat(structure) add removeLast method

--- module_base/homework_3/linkedList.js
+++ module_base/homework_3/linkedList.js
@@ -4,13 +4,12 @@ class Node {
         this.next = null;
         this.prev = null;
     }
-};
+}
 
 class LinkedList {
     constructor() {
         this.first = null;
         this.last = null;
-        this.nodesList = [];
     }
 
     add(data) {
@@ -19,9 +18,20 @@ class LinkedList {
             this.first = node;
         } else {
             this.last.next = node;
-            node.prev = this.last
+            node.prev = this.last;
         }
         this.last = node;
+    };
+
+    removeLast() {
+        if (this.last !== null) {
+            this.last = this.last.prev;
+            if (this.last !== null) {
+                this.last.next = null;
+            } else {
+                this.first = null;
+            }
+        }
     };
 
     *[Symbol.iterator]() {
@@ -32,7 +42,7 @@ class LinkedList {
             current = current.next;
         }
     };
-};
+}
 
 const list = new LinkedList();
 
@@ -45,6 +55,11 @@ console.log(list.last.value);            // 3
 console.log(list.first.next.value);      // 2
 console.log(list.first.next.prev.value); // 1
 
+list.removeLast();
+
+console.log(list.last.value);            // 2
+
+console.log('logs from iter');
 for (const value of list) {
     console.log(value);
 }
